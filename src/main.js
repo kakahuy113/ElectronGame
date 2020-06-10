f16f7bc38e8ed9af102be8f20821e8e87a0f264a
Huy Tran <kakahuy113@gmail.com>
2020-06-10 16:50:28 +0700

    upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+const electron = require("electron");
+const ipc = electron.ipcMain;
 
 const createWindow = () => {
-    let win = new BrowserWindow({
-        width: 1400, 
-        height: 1000,
-        webPreferences: { nodeIntegration: true }
-    })
-    win.loadFile('src/index.html');
-    //win.webContents.openDevTools();
+  let win = new BrowserWindow({
+    width: 1400,
+    height: 1000,
+    webPreferences: { nodeIntegration: true },
+  });
+  win.loadFile("src/index.html");
+  //win.webContents.openDevTools();
 
-    win.on('closed', () => {
-        win = null;
-    })
-}
+  win.on("closed", () => {
+    win = null;
+  });
+};
 
-app.on('ready', createWindow)
+const { ipcMain } = require("electron");
 
-app.on('window-all-closed', () => {
-    if (process.platform === 'darwin') app.quit();
-})
+ipcMain.on("lose", (event, arg) => {
+  event.returnValue = "You Lose !";
+});
+
+ipcMain.on("Again", (event, arg) => {
+  event.returnValue = "Play Again !";
+});
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform === "darwin") app.quit();
+});
